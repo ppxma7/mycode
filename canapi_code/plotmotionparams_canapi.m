b890abe33cee1f547c6621a74fbba9edd10f0c90
Michael Asghar <michael.asghar@nottingham.ac.uk>
2024-12-05 22:38:44 +0000

    update

--- canapi_code/plotmotionparams_canapi.m
+++ canapi_code/plotmotionparams_canapi.m
@@ -2,24 +2,22 @@
 clc
 close all
 clear variables
-thispath = '/Volumes/hermes/canapi_full_run_111024/spm_analysis/';
+thispath = '/Volumes/hermes/canapi_051224/spmanalysis/';
 
 userName = char(java.lang.System.getProperty('user.name'));
-savedir = ['/Users/' userName '/Library/CloudStorage/OneDrive-SharedLibraries-TheUniversityofNottingham/CANAPI Study (Ankle injury) - General/data/canapi_full_run_111024/plots/'];
+savedir = ['/Users/' userName '/Library/CloudStorage/OneDrive-SharedLibraries-TheUniversityofNottingham/CANAPI Study (Ankle injury) - General/data/canapi_051224/plots/'];
 
 
-myFiles = {'rp_parrec_WIP1bar_20241011131601_4_nordic_clv.txt',...
-    'rp_parrec_WIP1bar_20241011131601_10_nordic_clv.txt',...
-    'rp_parrec_WIP30prc_20241011131601_5_nordic_clv.txt',...
-    'rp_parrec_WIP30prc_20241011131601_11_nordic_clv.txt',...
-    'rp_parrec_WIP50prc_20241011131601_6_nordic_clv.txt',...
-    'rp_parrec_WIP50prc_20241011131601_12_nordic_clv.txt',...
-    'rp_parrec_WIP70prc_20241011131601_13_nordic_clv.txt'};
+myFiles = {'rp_parrec_WIP1bar_20241205082447_6_nordic_clv.txt',...
+    'rp_parrec_WIP30prc_20241205082447_5_nordic_clv.txt',...
+    'rp_parrec_WIP50prc_20241205082447_4_nordic_clv.txt',...
+    'rp_parrec_WIP1bar_20241205082447_10_nordic_clv.txt',...
+    'rp_parrec_WIP30prc_20241205082447_9_nordic_clv.txt',...
+    'rp_parrec_WIP50prc_20241205082447_8_nordic_clv.txt',...
+};
 
-nameFiles = {'1bar right leg','1bar left leg',...
-    '30 % right leg','30 % left leg',...
-    '50 % right leg','50 % left leg',...
-    '70 % left leg'};
+nameFiles = {'1bar run 1','30 % run 1','50 % run 1',...
+    '1 bar run 2','30 % run 2','50 % run 2'};
 
 for ii = 1:length(myFiles)
     plotRealignTextFiles([thispath myFiles{ii}],nameFiles{ii},savedir);
@@ -40,19 +38,37 @@ end
 % plotRealignTextFiles(textfile3, names3, savedir)
 
 %% plot global signal
-
-
+names1 = {'1bar run 1'};
+names2 = {'30% run 1'};
+names3 = {'50% run 1'};
+names4 = {'1bar run 2'};
+names5 = {'30% run 2'};
+names6 = {'70% run 2'};
 % Load the NIfTI files using niftiread
 
-filename1 = [thispath 'rcanapi_1bar_clv.nii'];
-filename2 = [thispath 'vrcanapi_1bar_clv.nii'];
+filename1 = [thispath 'rparrec_WIP1bar_20241205082447_6_nordic_clv.nii'];
+filename2 = [thispath 'vrparrec_WIP1bar_20241205082447_6_nordic_clv.nii'];
 plotGlobalSignal(filename1, filename2, names1, savedir)
 
-filename1 = [thispath 'rcanapi_30prc_clv.nii'];
-filename2 = [thispath 'vrcanapi_30prc_clv.nii'];
+filename1 = [thispath 'rparrec_WIP30prc_20241205082447_5_nordic_clv.nii'];
+filename2 = [thispath 'vrparrec_WIP30prc_20241205082447_5_nordic_clv.nii'];
 plotGlobalSignal(filename1, filename2, names2, savedir)
 
-filename1 = [thispath 'rcanapi_70prc_clv.nii'];
-filename2 = [thispath 'vrcanapi_70prc_clv.nii'];
+filename1 = [thispath 'rparrec_WIP50prc_20241205082447_4_nordic_clv.nii'];
+filename2 = [thispath 'vrparrec_WIP50prc_20241205082447_4_nordic_clv.nii'];
 plotGlobalSignal(filename1, filename2, names3, savedir)
+
+filename1 = [thispath 'rparrec_WIP1bar_20241205082447_10_nordic_clv.nii'];
+filename2 = [thispath 'vrparrec_WIP1bar_20241205082447_10_nordic_clv.nii'];
+plotGlobalSignal(filename1, filename2, names4, savedir)
+
+filename1 = [thispath 'rparrec_WIP30prc_20241205082447_9_nordic_clv.nii'];
+filename2 = [thispath 'vrparrec_WIP30prc_20241205082447_9_nordic_clv.nii'];
+plotGlobalSignal(filename1, filename2, names5, savedir)
+
+filename1 = [thispath 'rparrec_WIP50prc_20241205082447_8_nordic_clv.nii'];
+filename2 = [thispath 'vrparrec_WIP50prc_20241205082447_8_nordic_clv.nii'];
+plotGlobalSignal(filename1, filename2, names6, savedir)
+
+
 
