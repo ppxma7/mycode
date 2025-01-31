8695c9b7ff1718f7fdb09c2695a40993a85ee46a
Michael Asghar <michael.asghar@nottingham.ac.uk>
2025-01-31 20:53:09 +0000

    update

--- loop_convert_folder.sh
+++ loop_convert_folder.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Set input and output directories
-INPUT_DIR="/Users/spmic/data/SASHB"  # Change to actual path
-OUTPUT_DIR="/Users/spmic/data/SASHB/outputs"  # Change as needed
+#INPUT_DIR="/Users/spmic/data/NEXPO"  # Change to actual path
+#OUTPUT_DIR="/Users/spmic/data/NEXPO/outputs"  # Change as needed
+
+INPUT_DIR="/Volumes/nemosine/NEXPO"  # Change to actual path
+OUTPUT_DIR="/Volumes/nemosine/NEXPO/outputs"  # Change as needed
 
 # Loop through each subfolder inside INPUT_DIR
 for folder in "$INPUT_DIR"/*/; do
@@ -12,7 +15,7 @@ for folder in "$INPUT_DIR"/*/; do
     # Run dcm2niix and output to a temporary folder
     TEMP_DIR="${OUTPUT_DIR}/${folder_name}_tmp"
     mkdir -p "$TEMP_DIR"
-    dcm2niix -o "$TEMP_DIR" -f "%p_%s" "$folder"
+    /Applications/MRIcron.app/Contents/Resources/dcm2niix -o "$TEMP_DIR" -f "%p_%s" "$folder"
     
     # Create modality-specific folders
     mkdir -p "$OUTPUT_DIR/$folder_name/MPRAGE"
@@ -23,7 +26,7 @@ for folder in "$INPUT_DIR"/*/; do
     for file in "$TEMP_DIR"/*; do
         if [[ "$file" == *MPRAGE* ]]; then
             mv "$file" "$OUTPUT_DIR/$folder_name/MPRAGE/"
-        elif [[ "$file" == *T1mapping* ]]; then
+        elif [[ "$file" == *wh_1mm* ]]; then
             mv "$file" "$OUTPUT_DIR/$folder_name/T1mapping/"
         elif [[ "$file" == *FLAIR* ]]; then
             mv "$file" "$OUTPUT_DIR/$folder_name/FLAIR/"
