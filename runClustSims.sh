ce6917e2f1c1d0d3034c5534ca70e0ad8996289b
Michael Asghar <michael.asghar@nottingham.ac.uk>
2025-05-07 13:57:24 +0100

    update

---
+++ runClustSims.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+study=$1
+
+for meas in thickness volume; do
+  for hemi in lh rh; do
+    for smoothness in 10; do
+      dir="${hemi}.${meas}.${study}.${smoothness}.glmdir"
+      mri_glmfit-sim \
+        --glmdir "${dir}" \
+        --cache 3 abs \
+        --cwp 0.05 \
+        --2spaces
+    done
+  done
+done
