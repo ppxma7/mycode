c9b22dcb0503d20bac7065dfa57cff148191cf72
Michael Asghar <michael.asghar@nottingham.ac.uk>
2025-07-03 13:37:50 +0100

    update

--- ken_surf_testing_quick_plot.m
+++ ken_surf_testing_quick_plot.m
@@ -2,35 +2,220 @@ clear all
 close all
 clc
 
-subs = '020';
+subjectPath = '/Volumes/DRS-Touchmap/ma_ares_backup/TOUCH_REMAP/exp016/freesurfer/';
+setenv('SUBJECTS_DIR',subjectPath);
+
+dataPath = '/Volumes/nemosine/ken/';
+
+subs = {'026',...
+    '059'
+    };
 
 % should be where your subject's digit data are
-mypath = '/Users/spmic/data/ken_surf_testing/';
-savedir = fullfile(mypath,subs);
+%mypath = '/Users/spmic/data/ken_surf_testing/';
+savedir = dataPath;
 
-hemisphere = 'l';
+hemisphere = 'r';
 surface = 'inflated'; 
 map = 'viridis';
 nchips = 256;
 mymin = 0.5;
 myalpha = 1;
-
+zf = 1.8
 % set to where your freesurfer dirs are, e.g. 020 in this case
-subdir='/Users/spmic/data/subs/';
-setenv('SUBJECTS_DIR',subdir);
+% subdir='/Users/spmic/data/subs/';
+% setenv('SUBJECTS_DIR',subdir);
+%%
 
-% print each digit on the native surface one by one
-for ii = 1:5
-    theFile = MRIread(fullfile(mypath, subs, ['RD' num2str(ii) '_020.mgz']));
+for iSub = 1:length(subs)
+    clear data data_bin data_thresh
 
-    data = theFile.vol(:);
+    [vertices, faces] = read_surf(fullfile(subjectPath, subs{iSub}, 'surf', 'rh.inflated'));
+
+    for ii = 1:5
+        thisFile = ['LD' num2str(ii) '_' subs{iSub} '.mgz'];
+        theFile = MRIread(fullfile(dataPath, subs{iSub}, thisFile));
+        data(:,ii) = theFile.vol(:);
+
+        binariseData = data(:,ii)>0;
+
+        %data_bin(:,ii) = binariseData .* ii;
+
+    end
+
+    data_thresh = data;
+    data_thresh(data < 0.2) = 0;  % Only keep probabilities ≥ 0.2
+
+    % this creates a binary map, so setting each column to 1-5 
+    %Find the maximum probability and the digit it came from
+    [maxvals, maxdigit] = max(data_thresh, [], 2);  % data is X × 5
+
+    %Assign only voxels where the max is > 0
+    data_labels = zeros(size(maxdigit));        % default = 0 (unlabeled)
+    data_labels(maxvals > 0) = maxdigit(maxvals > 0);  % digit 1–5
+
+    %conflict_voxels = sum(data_labels > 0, 2) > 1;
+    %fprintf('Remaining overlaps: %d\n', sum(conflict_voxels));  % Should be 0 now
+
+    % we also want the FPM map combined
+    data_single_col = max(data_thresh, [], 2);
+
+    % Find max voxel index per digit
+    max_inds = zeros(5,1);
+    for d = 1:5
+        [~, max_inds(d)] = max(data_thresh(:, d));
+    end
+
+    %data_single_col = mean(data,2);
+
+    % seqmap = [27,158,119;...
+    %     217,95,2;...
+    %     117,112,179;...
+    %     231,41,138;...
+    %     102,166,30];
+    % seqmap = seqmap/255;
+    %
+    close all
+    figure
+    go_paint_freesurfer(data_single_col,...
+        subs{iSub},'r','range',...
+        [0.1 1], 'cbar','colormap',map)
+
+    hold on
+    % Plot dots at max digit points, adjusting marker size and color as you like
+    colors = digits(5);  % distinct colors for digits
+
+
+    [xs, ys, zs] = sphere(20); % sphere coordinates
+    h_sph = gobjects(5,1);  % to store handles for legend
+    r = 1.5; % radius of small sphere
+    for d = 1:5
+        h_sph(d) = surf(r*xs + vertices(max_inds(d),1), ...
+            r*ys + vertices(max_inds(d),2), ...
+            r*zs + vertices(max_inds(d),3), ...
+            'FaceColor', colors(d,:), 'EdgeColor', 'none');
+    end
+
+    legend(h_sph, {'D1', 'D2', 'D3', 'D4', 'D5'}, 'Location', 'northwestoutside')
+
+    hold off
+
+
+    camzoom(1.6)
+ 
+    print(fullfile(savedir, [subs{iSub} '_LD_digits_fpm']), '-dpdf', '-r600')
+    print(fullfile(savedir, [subs{iSub} '_LD_digits_fpm']), '-dpng', '-r600')
+
+%
+    seqmap = digits(5);
+    % plot this
+    clc
+    close all
 
     figure
-    go_paint_freesurfer(data,...
-        subs,'l','range',...
-        [0.1 1], 'cbar','colormap',map)
-    print(fullfile(savedir, sprintf('RD_%d_inflated', ii)), '-dpdf', '-r600')
+    go_paint_freesurfer(data_labels,...
+        subs{iSub},'r','range',...
+        [0.1 5], 'cbar','colormap',seqmap,'nchips',5,'customcmap')
+    camzoom(zf)
+
+    print(fullfile(savedir, [subs{iSub} '_LD_digits_bin']), '-dpdf', '-r600')
+    print(fullfile(savedir, [subs{iSub} '_LD_digits_bin']), '-dpng', '-r600')
 
 
 end
+
+
+
+
+
+
+
+
+
+%% testing this works
+% % print each digit on the native surface one by one
+% for iSub = 1:length(subs)
+%     for ii = 1:5
+%         thisFile = ['LD' num2str(ii) '_' subs{ii} '.mgz'];
+% 
+%         theFile = MRIread(fullfile(dataPath, subs{ii}, thisFile));
+% 
+%         data = theFile.vol(:);
+% 
+%         figure
+%         go_paint_freesurfer(data,...
+%             subs{ii},'r','range',...
+%             [0.1 1], 'cbar','colormap',map)
+%         %print(fullfile(savedir, sprintf('RD_%d_inflated', ii)), '-dpdf', '-r600')
+%         %keyboard
+% 
+%     end
+% end
+
+%% load in george atlas
+subdir = '/Users/ppzma/Documents/MATLAB/digitAtlas/subjects/';
+tic
+subs = {'001', '002', '003', '004', '005', '006', '007', '008', '009','010', ...
+    '011', '012', '013', '014', '015', '016', '017', '018', '019', '020', '021','022'};
+for ii = 1:length(subs)
+    for iDigit = 1:5
+        theFile = MRIread([subdir subs{ii} '/surf/LD' sprintf('%d',iDigit) '_manual' '.mgz']);
+        LD(:,iDigit,ii) = theFile.vol(:);
+    end  
+    for iDigit = 1:5
+        theFile = MRIread([subdir subs{ii} '/surf/RD' sprintf('%d',iDigit) '_manual' '.mgz']);
+        RD(:,iDigit,ii) = theFile.vol(:);
+    end
+end
+toc
+
+pLD_all = LD;
+pRD_all = RD;
+pLD_FPM = mean(LD,3);
+pRD_FPM = mean(RD,3);
+
+handAreaR = sum(pRD_FPM,2);
+handAreaL = sum(pLD_FPM,2);
+
+%%
+
+close all
+
+figure
+go_paint_freesurfer(handAreaL, ...
+    'fsaverage', 'r', ...
+    'range', [0.1 max(handAreaL)], ...
+    'cbar', 'colormap', 'viridis', ...
+    'OutlineOnly',1)
+%camzoom(zf)
+print(fullfile(savedir, 'digitatlasLDnozzom'), '-dpdf', '-r600')
+
+%print(fullfile(savedir, 'digitatlasLD'), '-dpng', '-r600')
+%print(fullfile(savedir, 'digitatlasLD'), '-dpdf', '-r600')
+
+figure
+go_paint_freesurfer(handAreaR, ...
+    'fsaverage', 'l', ...
+    'range', [0.1 max(handAreaR)], ...
+    'cbar', 'colormap', 'viridis', ...
+    'OutlineOnly',1)
+%camzoom(zf)
+print(fullfile(savedir, 'digitatlasRDnozzom'), '-dpdf', '-r600')
+
+%print(fullfile(savedir, 'digitatlasRD'), '-dpng', '-r600')
+%print(fullfile(savedir, 'digitatlasRD'), '-dpdf', '-r600')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
