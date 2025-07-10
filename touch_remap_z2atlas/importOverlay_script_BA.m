c20f70e56e452316c8d4c2bc1a0bb1df40e5c952
Michael Asghar <michael.asghar@nottingham.ac.uk>
2025-07-10 16:43:53 +0100

    up

--- touch_remap_z2atlas/importOverlay_script_BA.m
+++ touch_remap_z2atlas/importOverlay_script_BA.m
@@ -1,19 +1,19 @@
 %thisSub = '018';
-thisSub = '16227_mprage_1mm_freesurfer';
+thisSub = '14001';
 
 % theseOverlays = {'lh_3ab12_div.nii',...
 %     'rh_3ab12_div.nii'};
 
 %theseOverlays = {'lh.BA3a_exvivo.label.volume.nii'};
 
-theseOverlays = {'rh.BA3a_exvivo.label.volume.nii',...
-    'rh.BA3b_exvivo.label.volume.nii',...
-    'rh.BA1_exvivo.label.volume.nii',...
-    'rh.BA2_exvivo.label.volume.nii'};
+theseOverlays = {'lh.BA3a_exvivo.label.volume.nii',...
+    'lh.BA3b_exvivo.label.volume.nii',...
+    'lh.BA1_exvivo.label.volume.nii',...
+    'lh.BA2_exvivo.label.volume.nii'};
 
 
 %thispath = ['/Volumes/arianthe/exp016/freesurfer/' thisSub '/label/'];
-thispath = ['/Volumes/ares/subs/' thisSub '/label/'];
+thispath = ['/Volumes/DRS-TOUCHMAP/ma_ares_backup/subs/' thisSub '/label/'];
 %pathname = [thispath subject '/label/'];
 pathname = thispath;
 %pathname = [thispath subject '/ROIs/'];
@@ -43,9 +43,11 @@ for ii = 1:length(theseOverlays)
     v = viewGet([], 'view', 1);
     filename = theseOverlays{ii};
     %filename = 'lh.BA6_exvivo.label.volume.nii';
-    params.scanlist = 1; % 1 for forward, 2 for reverse
+    params.scanlist = 2; % 1 for forward, 2 for reverse
     params.min_overlay = 0.1;
-    %params.max_overlay = 10;
+    params.min_clip = 0.1;
+    params.max_clip = 10;
+    params.max_overlay = 10;
     params.pathname = [pathname filename];
     params.frameList = 1;
     params.useSform = 1;
