cf57eccace2a48a7a998ec186048cba6b4b10887
Michael Asghar <michael.asghar@nottingham.ac.uk>
2025-10-08 12:23:20 +0100

    up

---
+++ canapi_code/quickscript_canapisaving.m
@@ -0,0 +1,24 @@
+%%
+
+name = '1barR_neg';
+exportSPMtable(moo,name)
+spm_glass_quick(xSPM,name)
+%%
+
+name = 'lowR_neg';
+exportSPMtable(moo,name)
+spm_glass_quick(xSPM,name)
+%%
+
+name = '1barL_neg';
+exportSPMtable(moo,name)
+spm_glass_quick(xSPM,name)
+%%
+name = 'lowL_neg';
+exportSPMtable(moo,name)
+spm_glass_quick(xSPM,name)
+
+%%
+name = 'lowR_neg_level2_nomask';
+exportSPMtable(moo,name)
+spm_glass_quick(xSPM,name)
