d66e84b18761eb49fc63876749edd4945ac6da3a
Michael Asghar <michael.asghar@nottingham.ac.uk>
2025-11-06 13:09:19 +0000

    up

--- makeFolder.sh
+++ makeFolder.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+shopt -s nullglob extglob
 
-# Array of directories to be created
+# === Create directories if they don't exist ===
 dirs=("json" "magnitude" "phase" "parrec" "structurals" "DTI" "qa_outputs" "spm_analysis")
-
-# Create directories if they don't exist
 for dir in "${dirs[@]}"; do
     if [ -d "$dir" ]; then
         echo "Directory '$dir' already exists. Skipping creation."
@@ -12,60 +11,58 @@ for dir in "${dirs[@]}"; do
     fi
 done
 
-# Move JSON files
-if ls *.json &>/dev/null; then
-    mv -i *.json json/
+
+# === Move JSON files ===
+json_files=( *.json )
+if (( ${#json_files[@]} )); then
+    echo "Moving ${#json_files[@]} JSON files..."
+    mv -i "${json_files[@]}" json/
 else
     echo "No JSON files to move."
 fi
 
-# Move 
-if ls *MPRAGE*.nii.gz &>/dev/null; then
-    mv -i *MPRAGE*.nii.gz structurals/
-else
-    echo "No mprage files to move."
-fi
 
-# Move 
-if ls *FLAIR*.nii.gz &>/dev/null; then
-    mv -i *FLAIR*.nii.gz structurals/
-else
-    echo "No flair files to move."
-fi
+# === Move structural and DTI NIFTIs ===
+for prefix in MPRAGE FLAIR DTI; do
+    struct_files=( *${prefix}*.nii *${prefix}*.nii.gz )
+    if (( ${#struct_files[@]} )); then
+        echo "Moving ${#struct_files[@]} ${prefix} files..."
+        mv -i "${struct_files[@]}" "$( [[ $prefix == DTI ]] && echo DTI || echo structurals )"/
+    else
+        echo "No ${prefix} files to move."
+    fi
+done
 
-# Move 
-if ls *DTI*.nii.gz &>/dev/null; then
-    mv -i *DTI*.nii.gz DTI/
-else
-    echo "No dti files to move."
-fi
 
-# Move 
-if ls *_ph.nii.gz &>/dev/null; then
-    mv -i *_ph.nii.gz phase/
+# === Move phase files (_ph.nii or _ph.nii.gz) ===
+phase_files=(*_ph.nii *_ph.nii.gz)
+if (( ${#phase_files[@]} )); then
+    echo "Moving ${#phase_files[@]} phase files..."
+    mv -i "${phase_files[@]}" phase/
 else
     echo "No phase files to move."
 fi
 
-# Move remaining magnitude NIFTI files (*.nii.gz, excluding *_ph.nii.gz)
-shopt -s extglob
-if ls !(*_ph).nii.gz &>/dev/null; then
-    mv -i !(*_ph).nii.gz magnitude/
+
+# === Move magnitude files (everything else .nii/.nii.gz, excluding _ph) ===
+mag_files=(!(*_ph).nii !(*_ph).nii.gz)
+if (( ${#mag_files[@]} )); then
+    echo "Moving ${#mag_files[@]} magnitude files..."
+    mv -i "${mag_files[@]}" magnitude/
 else
     echo "No magnitude files to move."
 fi
-shopt -u extglob
 
-# Move PAR/REC files
-if ls *.PAR &>/dev/null; then
-    mv -i *.PAR parrec/
-else
-    echo "No PAR files to move."
-fi
 
-# Move PAR/REC files
-if ls *.REC &>/dev/null; then
-    mv -i *.REC parrec/
-else
-    echo "No REC files to move."
-fi
+# === Move PAR/REC files ===
+for ext in PAR REC; do
+    pr_files=( *.${ext} )
+    if (( ${#pr_files[@]} )); then
+        echo "Moving ${#pr_files[@]} ${ext} files..."
+        mv -i "${pr_files[@]}" parrec/
+    else
+        echo "No ${ext} files to move."
+    fi
+done
+
+shopt -u nullglob extglob
