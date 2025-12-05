0b38cfe14c32ef8e76290692a9f9d7d640cd32bc
Michael Asghar <michael.asghar@nottingham.ac.uk>
2025-12-05 14:26:46 +0000

    up

---
+++ makeFolder_movephase.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rootfolder="/Volumes/kratos/CHAINDTI"
+
+for subjdir in "$rootfolder"/*/; do
+    subj=$(basename "$subjdir")
+
+    echo "Processing subject: $subj"
+
+    # Define output directory INSIDE subject folder
+    outdir="${subjdir}/phasefiles"
+
+    echo $outdir
+
+    # Create it if it doesn't exist
+    mkdir -p "$outdir"
+
+    # Move files (only if they exist)
+    mv "$subjdir"/*_ph* "$outdir"/ 
+    #mv "$subjdir"/*.json "$outdir"/ 2>/dev/null
+
+    echo "Completed subject: $subj"
+    echo "----------------------------"
+done
