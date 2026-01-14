43278a16007914e48433cbec5527ae782269ce61
Michael Asghar <michael.asghar@nottingham.ac.uk>
2026-01-14 13:29:42 +0000

    up

--- dti_code/compareDTI_nexpo_chain.m
+++ dti_code/compareDTI_nexpo_chain.m
@@ -1,3 +1,5 @@
+
+userName = char(java.lang.System.getProperty('user.name'));
 savedgroup = 'compare_dti_values_between_scan_sequences';
 mypath = ['/Users/' userName '/Library/CloudStorage/OneDrive-SharedLibraries-TheUniversityofNottingham/Michael_Sue - General/AFIRM_SASHB_NEXPO/dti_data/' savedgroup '/'];
 
@@ -12,25 +14,33 @@ vm_chain_fa = vm_chain.vm;
 vm_nexpo_md  = vm_nexpo_md.vm;
 vm_chain_md = vm_chain_md.vm;
 
-vmall = [vm_nexpo_fa; vm_chain_fa];
-vmallmd = [vm_nexpo_md; vm_chain_md];
+vm_justchain_fa = vm_chain_fa(1:14); % first 14 subs are CHAIN
+vm_justchain_md = vm_chain_md(1:14);
 
-grp_nexpo = repmat({'NEXPO'},length(vm_nexpo_data),1);
-grp_chain = repmat({'CHAINAFIRMSASHB'},length(vm_chain_data),1);
+% remove first 28 (nexpo g1, as they're young)
+% we only have 191 for DTI, loo at NEXPOAGESRAW_DTI.xlsx
+vm_justg2_fa = vm_nexpo_fa(49:end);
+vm_justg2_md = vm_nexpo_md(49:end);
+
+vmall = [vm_justg2_fa; vm_justchain_fa];
+vmallmd = [vm_justg2_md; vm_justchain_md];
+
+grp_nexpo = repmat({'NEXPO'},length(vm_justg2_fa),1);
+grp_chain = repmat({'CHAIN'},length(vm_justchain_fa),1);
 
 grpall = [grp_nexpo; grp_chain];
 
 % Process the loaded data (e.g., compute the mean of each dataset)
-mean_nexpo = mean(vm_nexpo_fa);
-mean_chain = mean(vm_chain_fa);
+mean_nexpo = mean(vm_justg2_fa);
+mean_chain = mean(vm_justchain_fa);
 
 % Display the computed means
 disp(['Mean of vm_nexpo FA: ', num2str(mean_nexpo)]);
 disp(['Mean of vm_chain FA: ', num2str(mean_chain)]);
 
 % Process the loaded data (e.g., compute the mean of each dataset)
-mean_nexpo = mean(vm_nexpo_md);
-mean_chain = mean(vm_chain_md);
+mean_nexpo = mean(vm_justg2_md);
+mean_chain = mean(vm_justchain_md);
 
 % Display the computed means
 disp(['Mean of vm_nexpo MD: ', num2str(mean_nexpo)]);
