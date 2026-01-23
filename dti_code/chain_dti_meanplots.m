121a1afe00797e52fcb68db7f0cacec03e312e8c
Michael Asghar <michael.asghar@nottingham.ac.uk>
2026-01-23 10:32:45 +0000

    up

---
+++ dti_code/chain_dti_meanplots.m
@@ -0,0 +1,11 @@
+thisType = 'FA';
+root = ['/Volumes/DRS-CHAIN-Study/dti_data/dti_' thisType '/'];
+
+savedgroup = 'chain_dti';
+
+userName = char(java.lang.System.getProperty('user.name'));
+savedir = ['/Users/' userName '/Library/CloudStorage/OneDrive-SharedLibraries-TheUniversityofNottingham/Michael_Sue - General/' savedgroup '/'];
+
+
+files = dir(root);
+
